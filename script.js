ebb7a86f4b6a3483aba89bfd6ed260c0113e58f9
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-01 08:31:50 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function firstChar(text) {
   // your code here
-	
-  for (let i = 0; i < text.length; i++) {
+	 for (let i = 0; i < text.length; i++) {
     // Check if the character is not a space
     if (text[i] !== ' ') {
       return text[i];
@@ -11,7 +10,6 @@ function firstChar(text) {
   return '';
 }
 
-// Do not change the code below
-//Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
-alert(firstChar(text));
+// Correct way to handle the prompt and pass text to the function
+const text = prompt("Enter text:"); // Get input from the user
+alert(firstChar(text)); 

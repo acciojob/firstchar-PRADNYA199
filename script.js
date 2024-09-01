46340663a4fca974445dfed031e56384ba4b8e8a
Pradnya Chandrakant Bhosale <67332759+PRADNYA199@users.noreply.github.com>
2024-09-01 08:28:41 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function firstChar(text) {
   // your code here
-	for(let i=0;i<text.length;i++)
-		{
-			if(text[i]!== ' ')
-			{
-				return text[i];
-			}
-		}
-	return ' ';
+	
+  for (let i = 0; i < text.length; i++) {
+    // Check if the character is not a space
+    if (text[i] !== ' ') {
+      return text[i];
+    }
+  }
+  // If no non-space character is found, return an empty string
+  return '';
 }
 
 // Do not change the code below
